0dbe56d8f38ecb51e74daf8d8e57df4c16c99feb
wonder1ng <wonder1ng@velog.io>
2024-08-14 17:59:08 +0900

    updated

--- practice_03.py
+++ practice_03.py
@@ -1,1 +1,488 @@
 # 10장
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
+
+iris = load_iris()
+X = iris.data[:, (2, 3)]
+y = (iris.target == 0).astype(np.int8)
+
+per_clf = Perceptron()
+per_clf.fit(X, y)
+
+print(per_clf.predict([[2, 0.5]]))
+import tensorflow as tf
+from tensorflow import keras
+
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+
+print(X_train_full.shape, X_train_full.dtype)
+
+X_valid, X_train = X_train_full[:5000] / 255, X_train_full[5000:] / 255
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+X_test = X_test / 255
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# 모델에 대해 설명
+
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))
+model.add(keras.layers.Dense(300, activation='relu'))
+model.add(keras.layers.Dense(100, activation='relu'))
+model.add(keras.layers.Dense(10, activation='softmax'))
+model.summary()
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dense(300, activation='relu'),
+    keras.layers.Dense(100, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+model.summary()
+keras.utils.plot_model(model, show_shapes=True)
+print(model.layers)
+hidden1 = model.layers[1]
+print(hidden1.name)
+print(model.get_layer(hidden1.name) is hidden1)  # 'dense_3'
+
+weights, biases = hidden1.get_weights()
+print(weights)
+print(weights.shape)
+print(biases)
+print(biases.shape)
+model.compile(loss='sparse_categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+# 컴파일에 대해 설명
+history = model.fit(X_train, y_train, batch_size=1028, epochs=30, validation_data=(X_valid, y_valid))
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.DataFrame(history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)
+plt.show(block=False)
+plt.pause(2)
+plt.close()
+print(model.evaluate(X_test, y_test))
+X_new = X_test[:3]
+y_proba = model.predict(X_new)
+y_proba.round(2)
+y_pred = np.argmax(y_proba, axis=1)
+print(y_pred)
+print(np.array(class_names)[y_pred])
+
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+housing = fetch_california_housing()
+X_train_full, X_test, y_train_full, y_test = train_test_split(housing.data, housing.target)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_valid = scaler.transform(X_valid)
+X_test = scaler.transform(X_test)
+model = keras.models.Sequential([
+    keras.layers.Dense(30, activation='relu', input_shape=X_train.shape[1:]),
+    keras.layers.Dense(1)
+])
+model.compile(loss='mean_squared_error', optimizer='sgd')
+history = model.fit(X_train, y_train, epochs=20,
+                    validation_data=(X_valid, y_valid))
+
+
+mse_test = model.evaluate(X_test, y_test)
+X_new = X_test[:3]
+y_pred = model.predict(X_new)
+
+
+input_ = keras.layers.Input(shape=X_train.shape[1:])
+hidden1 = keras.layers.Dense(30, activation='relu')(input_)
+hidden2 = keras.layers.Dense(30, activation='relu')(hidden1)
+concat = keras.layers.Concatenate()([input_, hidden2])
+output = keras.layers.Dense(1)(concat)
+model = keras.Model(inputs=[input_], outputs=[output])
+
+model.compile('SGD', 'mse')
+
+model.fit(X_train, y_train, 1028, 20, validation_data=(X_valid, y_valid))
+
+model.evaluate(X_test, y_test)
+
+
+input_A = keras.layers.Input(shape=[5], name='wide_input')  # short_path
+input_B = keras.layers.Input(shape=[6], name='deep_input')
+hidden1 = keras.layers.Dense(30, activation='relu')(input_B)
+hidden2 = keras.layers.Dense(30, activation='relu')(hidden1)
+concat = keras.layers.concatenate([input_A, input_B])
+output = keras.layers.Dense(1, name='output')(concat)
+model = keras.Model(inputs=[input_A, input_B], outputs=output)
+
+model.compile(loss='mse', optimizer=keras.optimizers.SGD(learning_rate=1e-3))
+
+X_train_A, X_train_B = X_train[:, :5], X_train[:, 2:]
+X_valid_A, X_valid_B = X_valid[:, :5], X_valid[:, 2:]
+X_test_A, X_test_B = X_test[:, :5], X_test[:, 2:]
+X_new_A, X_new_B = X_test_A[:3], X_test_B[:3]
+
+history = model.fit((X_train_A, X_train_B), y_train, epochs=20,
+                    validation_data=((X_valid_A, X_valid_B), y_valid))
+mse_test = model.evaluate((X_test_A, X_test_B), y_test)
+y_pred = model.predict((X_new_A, X_new_B))
+
+
+input_A = keras.layers.Input(shape=[5], name='wide_input')  # short_path
+input_B = keras.layers.Input(shape=[6], name='deep_input')
+hidden1 = keras.layers.Dense(30, activation='relu')(input_B)
+hidden2 = keras.layers.Dense(30, activation='relu')(hidden1)
+concat = keras.layers.concatenate([input_A, input_B])
+output = keras.layers.Dense(1, name='main_output')(concat)
+aux_output = keras.layers.Dense(1, name='aux_output')(hidden2)
+model = keras.Model(inputs=[input_A, input_B], outputs=[output, aux_output])
+
+model.compile(loss=['mse', 'mse'], loss_weights=[0.9, 0.1], optimizer=keras.optimizers.SGD(learning_rate=1e-3))
+
+history = model.fit((X_train_A, X_train_B), [y_train, y_train], epochs=20,
+                    validation_data=((X_valid_A, X_valid_B), [y_valid, y_valid]))
+total_loss, main_loss, aux_loss = model.evaluate((X_test_A, X_test_B), [y_test, y_test])
+print(total_loss, main_loss, aux_loss)
+
+y_pred_main, y_pred_aux = model.predict((X_new_A, X_new_B))
+print(y_pred_main, y_pred_aux, sep='\n\n')
+
+
+class WideAndDeepModel(keras.Model):
+    def __init__(self, units=30, activation='relu', **kwargs):
+        super().__init__(**kwargs)
+        self.hidden1 = keras.layers.Dense(units, activation=activation)
+        self.hidden2 = keras.layers.Dense(units, activation=activation)
+        self.main_output = keras.layers.Dense(1)
+        self.aux_output = keras.layers.Dense(1)
+
+    def call(self, inputs):
+        input_A, input_B = inputs
+        hidden1 = self.hidden1(input_B)
+        hidden2 = self.hidden2(hidden1)
+        concat = keras.layers.concatenate([input_A, hidden2])
+        main_output = self.main_output(concat)
+        aux_output = self.aux_output(hidden2)
+        return main_output, aux_output
+
+
+model = WideAndDeepModel()
+model.compile(loss="mse", loss_weights=[0.9, 0.1], optimizer=keras.optimizers.SGD(learning_rate=1e-3))
+history = model.fit((X_train_A, X_train_B), (y_train, y_train), epochs=10,
+                    validation_data=((X_valid_A, X_valid_B), (y_valid, y_valid)))
+total_loss, main_loss, aux_loss = model.evaluate((X_test_A, X_test_B), [y_test, y_test])
+print(total_loss, main_loss, aux_loss)
+
+y_pred_main, y_pred_aux = model.predict((X_new_A, X_new_B))
+print(y_pred_main, y_pred_aux, sep='\n\n')
+
+
+input_A = keras.layers.Input(shape=[5], name='wide_input')  # short_path
+input_B = keras.layers.Input(shape=[6], name='deep_input')
+hidden1 = keras.layers.Dense(30, activation='relu')(input_B)
+hidden2 = keras.layers.Dense(30, activation='relu')(hidden1)
+concat = keras.layers.concatenate([input_A, input_B])
+output = keras.layers.Dense(1, name='main_output')(concat)
+aux_output = keras.layers.Dense(1, name='aux_output')(hidden2)
+model = keras.Model(inputs=[input_A, input_B], outputs=[output, aux_output])
+
+model.compile(loss=['mse', 'mse'], loss_weights=[0.9, 0.1], optimizer=keras.optimizers.SGD(learning_rate=1e-3))
+
+history = model.fit((X_train_A, X_train_B), [y_train, y_train], epochs=20,
+                    validation_data=((X_valid_A, X_valid_B), [y_valid, y_valid]))
+
+model.save('my_keras_model.h5')
+model = keras.models.load_model('my_keras_model.h5')
+y_pred_main, y_pred_aux = model.predict((X_new_A, X_new_B))
+print(y_pred_main, y_pred_aux, sep='\n\n')
+
+
+# # API 모델
+# model.save(path + model_name.h5)
+# model = keras.models.load_model(path + model_name.h5)
+
+# # 서브클래싱 모델
+# # 모델 전체 저장
+# model.save(path + dir_name, save_format='tf')
+# model = keras.models.load_model(path + dir_name, custom_objects={'SubClassing': SubClassing})
+
+# # 모델 가중치 저장 ※ 확장자는 ckpt
+# model.save_weights(path + model_name.ckpt)
+# # 동일한 구조로 모델 정의
+# model = SubClassing()
+# model.compile(loss="mse", loss_weights=[0.9, 0.1], optimizer='SGD')
+# model.load_weights(path + model_name.ckpt)
+
+# # 확장자를 h5를 썼을 경우에는 fit을 해야지만 load 가능 내부 변수 어쩌구하는데 이유는 모름. gpt도 모름
+# model.save_weights(path + model_name.h5)
+# # 동일한 구조로 모델 정의
+# model = SubClassing()
+# model.compile(loss="mse", loss_weights=[0.9, 0.1], optimizer='SGD')
+# # 최소 조건으로 fit
+# history = model.fit((X_train_A[:1], X_train_B[:1]), (y_train[:1], y_train[:1]), epochs=1)
+# # 가중치 로드
+# model.load_weights(path + model_name.h5)
+
+# y_pred_main, y_pred_aux = model.predict((X_new_A, X_new_B))
+# print(y_pred_main, y_pred_aux, sep='\n\n')
+
+
+class PrintValTrainRatioCallback(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        print('\nval/train: {:.2f}'.format(logs['val_loss'] / logs['loss']))
+
+# 아래의 함수를 이용해서 callback 생성 가능
+# on_batch_begin, on_batch_end,
+# on_epoch_begin, on_epoch_end,
+# on_predict_batch_begin, on_predict_batch_end, on_predict_begin, on_predict_end,
+# on_test_batch_begin, on_test_batch_end, on_test_begin, on_test_end,
+# on_train_batch_begin, on_train_batch_end, on_train_begin, on_train_end,
+
+
+
+class WideAndDeepModel(keras.Model):
+    def __init__(self, units=30, activation='relu', **kwargs):
+        super().__init__(**kwargs)
+        self.hidden1 = keras.layers.Dense(units, activation=activation)
+        self.hidden2 = keras.layers.Dense(units, activation=activation)
+        self.main_output = keras.layers.Dense(1)
+        self.aux_output = keras.layers.Dense(1)
+
+    def call(self, inputs):
+        input_A, input_B = inputs
+        hidden1 = self.hidden1(input_B)
+        hidden2 = self.hidden2(hidden1)
+        concat = keras.layers.concatenate([input_A, hidden2])
+        main_output = self.main_output(concat)
+        aux_output = self.aux_output(hidden2)
+        return main_output, aux_output
+
+
+model = WideAndDeepModel()
+model.compile(loss="mse", loss_weights=[0.9, 0.1], optimizer=keras.optimizers.SGD(learning_rate=1e-3))
+
+checkpoint_cb = keras.callbacks.ModelCheckpoint('my_keras_model.ckpt')
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=1, restore_best_weights=True)
+custom_callback = PrintValTrainRatioCallback()
+
+history = model.fit((X_train_A, X_train_B), (y_train, y_train), epochs=100,
+                    validation_data=((X_valid_A, X_valid_B), (y_valid, y_valid)),
+                    callbacks=[checkpoint_cb, early_stopping_cb, custom_callback])
+total_loss, main_loss, aux_loss = model.evaluate((X_test_A, X_test_B), [y_test, y_test])
+print(total_loss, main_loss, aux_loss)
+
+y_pred_main, y_pred_aux = model.predict((X_new_A, X_new_B))
+print(y_pred_main, y_pred_aux, sep='\n\n')
+
+
+import os
+
+root_logdir = os.path.join(os.curdir, 'my_logs')
+
+
+def get_run_logdir():
+    import time
+    run_id = time.strftime("run_%Y_%m_%d-%H_%M_%S")
+
+    return os.path.join(root_logdir, run_id)
+
+
+run_logdir = get_run_logdir()
+run_logdir
+
+
+class WideAndDeepModel(keras.Model):
+    def __init__(self, units=30, activation='relu', **kwargs):
+        super().__init__(**kwargs)
+        self.hidden1 = keras.layers.Dense(units, activation=activation)
+        self.hidden2 = keras.layers.Dense(units, activation=activation)
+        self.main_output = keras.layers.Dense(1)
+        self.aux_output = keras.layers.Dense(1)
+
+    def call(self, inputs):
+        input_A, input_B = inputs
+        hidden1 = self.hidden1(input_B)
+        hidden2 = self.hidden2(hidden1)
+        concat = keras.layers.concatenate([input_A, hidden2])
+        main_output = self.main_output(concat)
+        aux_output = self.aux_output(hidden2)
+        return main_output, aux_output
+
+
+model = WideAndDeepModel()
+model.compile(loss="mse", loss_weights=[0.9, 0.1], optimizer=keras.optimizers.SGD(learning_rate=1e-3))
+
+# 텐서보드 콜백
+tensornoard_cb = keras.callbacks.TensorBoard(run_logdir)
+history = model.fit((X_train_A, X_train_B), (y_train, y_train), batch_size=1028, epochs=30,
+                    validation_data=((X_valid_A, X_valid_B), (y_valid, y_valid)),
+                    callbacks=[tensornoard_cb])
+
+
+test_logdir = get_run_logdir()
+writer = tf.summary.create_file_writer(test_logdir)
+with writer.as_default():
+    for step in range(1, 1000 + 1):
+        tf.summary.scalar('my_scalar', np.sin(step / 10), step=step)
+        data = (np.random.randn(100) + 2) * step / 100
+        tf.summary.histogram('my_hist', data, buckets=50, step=step)
+        images = np.random.rand(2, 32, 32, 3)  # 32*32 RGB 이미지
+        tf.summary.image('my_images', images * step / 1000, step=step)
+        texts = ['The step is ' + str(step), 'Its square is ' + str(step ** 2)]
+        tf.summary.text('my_text', texts, step=step)
+        sine_wave = tf.math.sin(tf.range(12000) / 48000 * 2 * np.pi * step)
+        audio = tf.reshape(tf.cast(sine_wave, tf.float32), [1, -1, 1])
+        tf.summary.audio('my_audio', audio, sample_rate=48000, step=step)
+
+
+def build_model(n_hidden=1, n_neurons=30, learning_rate=3e-3, input_shape=[8]):
+    model = keras.models.Sequential()
+    model.add(keras.layers.InputLayer(input_shape=input_shape))
+    for layer in range(n_hidden):
+        model.add(keras.layers.Dense(n_neurons, activation='relu'))
+    model.add(keras.layers.Dense(1))
+    optimizer = keras.optimizers.SGD(learning_rate=learning_rate)
+    model.compile(loss='mse', optimizer=optimizer)
+
+    return model
+
+
+keras_reg = keras.wrappers.scikit_learn.KerasRegressor(build_model)
+# keras.wrappers.scikit_learn: Keras 모델을 Scikit-Learn의 API와 호환되도록 만드는 유틸리티
+
+keras_reg.fit(X_train, y_train, epochs=100,
+              validation_data=(X_valid, y_valid),
+              callbacks=[keras.callbacks.EarlyStopping(patience=10)])
+mse_test = keras_reg.score(X_test, y_test)
+y_pred = keras_reg.predict(X_new)
+print(mse_test)
+
+
+from scipy.stats import reciprocal
+from sklearn.model_selection import RandomizedSearchCV
+
+param_distribs = {
+    'n_hidden': [0, 1, 2, 3],
+    # 'n_neurons': np.arange(1,100),
+    'n_neurons': [2 ** n for n in range(8)],
+    'learning_rate': reciprocal(3e-4, 3e-2)
+}
+
+rnd_search_cv = RandomizedSearchCV(keras_reg, param_distribs, n_iter=10, cv=3)
+rnd_search_cv.fit(X_train, y_train, batch_size=1028, epochs=100,
+                  validation_data=(X_valid, y_valid), verbose=0,
+                  callbacks=[keras.callbacks.EarlyStopping(patience=10)])
+
+print(rnd_search_cv.best_params_)
+print(rnd_search_cv.best_score_)
+print(rnd_search_cv.best_estimator_)
+model = rnd_search_cv.best_estimator_.model
+
+
+
+
+(X_train_full, y_train_full), (X_test, y_test) = keras.datasets.mnist.load_data()
+
+X_valid, X_train = X_train_full[:5000] / 255., X_train_full[5000:] / 255.
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+X_test = X_test / 255.
+
+
+# K = keras.backend
+
+class ExponentialLearningRate(keras.callbacks.Callback):
+    def __init__(self, factor):
+        self.factor = factor  # 곱할 값
+        self.rates = []  # 학습률 값 저장
+        self.losses = []  # loss 값 저장
+
+    def on_batch_end(self, batch, logs):
+        # self.rates.append(K.get_value(self.model.optimizer.lr))
+        self.rates.append(model.optimizer.lr.numpy())
+        self.losses.append(logs["loss"])
+        # K.set_value(self.model.optimizer.lr, self.model.optimizer.lr * self.factor)
+        self.model.optimizer.lr = self.model.optimizer.lr * self.factor
+
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dense(300, activation="relu"),
+    keras.layers.Dense(100, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=keras.optimizers.SGD(learning_rate=1e-3),
+              metrics=["accuracy"])
+expon_lr = ExponentialLearningRate(factor=1.005)
+history = model.fit(X_train, y_train, epochs=1,
+                    validation_data=(X_valid, y_valid),
+                    callbacks=[expon_lr])
+
+
+plt.plot(expon_lr.rates, expon_lr.losses)
+plt.gca().set_xscale('log')
+plt.hlines(min(expon_lr.losses), min(expon_lr.rates), max(expon_lr.rates))
+plt.axis([min(expon_lr.rates), max(expon_lr.rates), 0, expon_lr.losses[0]])
+plt.grid()
+plt.xlabel("Learning rate")
+plt.ylabel("Loss");
+# 풀이에선 0.6부터 상승하여 0.3을 learning rate로 사용한다 하였는데
+# 403쪽에서는 상승점보다 약 10배 낮은 지점이라 하여 min_rate/10을 비교
+min_rate = expon_lr.rates[expon_lr.losses.index(min(expon_lr.losses))]
+print(min_rate)
+
+
+run_index = 1
+run_logdir = os.path.join(os.curdir, "my_mnist_logs", "run_{:03d}".format(run_index))
+print(run_logdir)
+
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dense(300, activation="relu"),
+    keras.layers.Dense(100, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=keras.optimizers.SGD(learning_rate=3e-1),
+              metrics=["accuracy"])
+
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=20)
+checkpoint_cb = keras.callbacks.ModelCheckpoint("my_mnist_model.h5", save_best_only=True)
+tensorboard_cb = keras.callbacks.TensorBoard(run_logdir)
+
+history = model.fit(X_train, y_train, epochs=100,
+                    validation_data=(X_valid, y_valid),
+                    callbacks=[checkpoint_cb, early_stopping_cb, tensorboard_cb])
+
+
+run_index = 2  # 실행할 때마다 이 값을 늘립니다
+run_logdir = os.path.join(os.curdir, "my_mnist_logs", "run_{:03d}".format(run_index))
+
+tensorboard_cb = keras.callbacks.TensorBoard(run_logdir)
+
+model2 = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dense(300, activation="relu"),
+    keras.layers.Dense(100, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model2.compile(loss="sparse_categorical_crossentropy",
+               optimizer=keras.optimizers.SGD(learning_rate=min_rate / 10),
+               metrics=["accuracy"])
+
+history2 = model2.fit(X_train, y_train, epochs=100,
+                      validation_data=(X_valid, y_valid),
+                      callbacks=[checkpoint_cb, early_stopping_cb, tensorboard_cb])
+print(model.evaluate(X_test, y_test), model2.evaluate(X_test, y_test))
+# 3~4번 돌려봤는데 동일 및 최종 에폭 기준 0.3이 loss는 나쁘고 accuracy는 좋음
